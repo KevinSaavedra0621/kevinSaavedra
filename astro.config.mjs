ee9637ec2b1b2c32f19b9737a2933da8de0a05f0
KevinSaavedra0621 <kesaavedraortiz@utede.edu.co>
2025-11-14 15:04:16 -0500

    actualizacion astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from 'astro/config'
+vite: {
+  plugins:[tailwindcss()],
+site: 'https://kevinSaavedra.github.io'
+}
 // https://astro.build/config
 export default defineConfig({});
